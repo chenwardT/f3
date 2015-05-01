1bea6f29b2f6fd99f99ee81c8ced9ac958c131a8
Edward Chen <chenward.t@gmail.com>
2015-05-01 10:44:19 -0400

    Decided against per-post replies.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
-  belongs_to :reply_to, class_name: 'Post'
+  # belongs_to :reply_to, class_name: 'Post'
 
-  has_many :replies, class_name: 'Post',
-                     foreign_key: 'reply_to_id',
-                     dependent: :nullify
+  # has_many :replies, class_name: 'Post',
+  #                    foreign_key: 'reply_to_id',
+  #                    dependent: :nullify
 end
--- db/migrate/20150430215349_add_replies_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddRepliesToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :reply_to_id, :integer
-  end
-end
