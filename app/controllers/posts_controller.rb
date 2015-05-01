eefd2945d93b5c87b9fd887cdfd504b41442ca57
Edward Chen <chenward.t@gmail.com>
2015-05-01 17:34:47 -0400

    Posting replies to topics - form displays if logged in. Flash messages styled.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.topic_id = params[:topic_id]
+
+    if @post.save
+      flash[:success] = "Post successfully created"
+    else
+      flash[:danger] = "Error posting reply"
+    end
+
+    redirect_to topic_path(@post.topic)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,5 +5,6 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
+    @post = Post.new
   end
 end
