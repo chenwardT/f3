180b2d7ebfd4479c9cde678800cc8a5fffcf670c
Edward Chen <chenward.t@gmail.com>
2015-05-04 13:35:14 -0400

    Cleanup Post creation (replying to a thread).

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,22 +4,28 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user = current_user
-    @post.topic_id = params[:topic_id]
+    @post = current_user.posts.build(post_params)
 
     if @post.save
       flash[:success] = "Post successfully created"
+      redirect_to topic_path(params[:topic_id])
     else
       flash[:danger] = "Error posting reply"
+      render topic_path(params[:topic_id])
     end
-
-    redirect_to topic_path(@post.topic)
   end
 
   private
 
+  # TODO: Is this OK?
+  # ex. incoming parameters:
+  # {"utf8"=>"✓",
+  #  "authenticity_token"=>"OMkmu7fy9+uHIVSPqKIEJA8AXO9ZaghkvwneHXBg1UcAIjnJo8LTIWLQ4s0pSJZpVy2RKhiE9A/3bUnhuuuY2Q==",
+  #  "topic_id"=>"5",
+  #  "post"=>{"body"=>"asdasdas"},
+  #  "commit"=>"Post"}
+
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body).merge({topic_id: params.require(:topic_id)})
   end
 end
