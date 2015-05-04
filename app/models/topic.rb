2a74c2c04a66bdc7cdacd19a5e962412af8236d9
Edward Chen <chenward.t@gmail.com>
2015-05-04 17:23:38 -0400

    Forum#show table displays title + author + date, # replies, last post user + date.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,12 @@ class Topic < ActiveRecord::Base
   belongs_to :user
 
   has_many :posts
+
+  def reply_count
+    posts.count - 1
+  end
+
+  def last_post
+    posts.order(created_at: :asc).last
+  end
 end
