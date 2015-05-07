6710449eea3bbd3e218f619f536910f43fab2872
Edward Chen <chenward.t@gmail.com>
2015-05-07 16:01:58 -0400

    Topic displaying posts in order.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,11 +7,16 @@ class Topic < ActiveRecord::Base
   def to_s
     title
   end
+
   def reply_count
     posts.count - 1
   end
 
   def last_post
     posts.order(created_at: :asc).last
+  end
+
+  def ordered_posts
+    posts.order(created_at: :asc)
   end
 end
