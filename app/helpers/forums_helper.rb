457ab23c4936550f1cb0638e8fd1c794fb2c85a6
Edward Chen <chenward.t@gmail.com>
2015-05-08 02:53:45 -0400

    ForumsHelper#subforum_links generated (by default) comma separated links to subforums, given a forum.

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,2 +1,11 @@
 module ForumsHelper
+  def subforum_links(forum, separator=', ')
+    links = []
+
+    forum.forums.each do |subforum|
+      links.push('<a href="' + forum_path(subforum) + '">' + subforum.title + '</a>')
+    end
+
+    links.join(separator).html_safe
+  end
 end
