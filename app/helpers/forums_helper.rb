c6c4f8a4a4927f41eb026a324111c723224730c5
Edward Chen <chenward.T@gmail.com>
2015-07-26 10:40:54 -0400

    Added last post to forums#index. Fixed subforum topic/post counts on forums#index. Renamed display_last_post to display_last_post_with_topic.

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -9,7 +9,7 @@ module ForumsHelper
     links.join(separator).html_safe
   end
 
-  def display_last_post(topic, post)
+  def display_last_post_with_topic(topic, post)
     topic_html = content_tag(:a, topic, href: topic_path(topic) + '?page=' + topic.num_pages.to_s + '#reply')
     author_html = content_tag(:a, post.user.username, href: user_path(post.user))
     date_html = content_tag(:small, post.created_at)
