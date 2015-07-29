a2aad6c29696e2c12d8832d9f3824145a46edd69
Edward Chen <chenward.T@gmail.com>
2015-07-29 19:28:08 -0400

    Start model tests: forum, user

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ForumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'forum hierarchy' do
+    assert_equal forums(:cars).forums, [forums(:na), forums(:hybrids), forums(:electric)]
+    assert_includes forums(:hybrids).forums, forums(:prius)
+    assert_includes forums(:electric).forums, forums(:model_s)
+    assert_equal forums(:model_s).forums, [forums(:model_s_2014), forums(:model_s_2015)]
+  end
+
+  test 'to_s returns title' do
+    assert_equal forums(:cars).to_s, forums(:cars).title
+  end
+
+  test 'topic_count returns count of nested topics' do
+    assert_equal forums(:cars).topic_count, forums(:cars).topics.count
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'user to_s returns username' do
+    assert_equal users(:tom).to_s, users(:tom).username
+  end
 end
