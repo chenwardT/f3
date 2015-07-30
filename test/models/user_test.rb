2d8ca4bda92b2d1180bc9eb4d82371bc80c545ce
Edward Chen <chenward.T@gmail.com>
2015-07-30 15:14:55 -0400

    Test User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   test 'user to_s returns username' do
     assert_equal users(:tom).to_s, users(:tom).username
   end
+
+  test 'newest scope should return last created username' do
+    assert_equal User.newest, users(:alice).username
+  end
 end
