3176f02a024444b4fa055209ca4fdc0868d1d248
Edward Chen <chenward.T@gmail.com>
2015-07-30 15:15:20 -0400

    Test Forum model methods and scope

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -15,4 +15,43 @@ class ForumTest < ActiveSupport::TestCase
   test 'topic_count returns count of nested topics' do
     assert_equal forums(:cars).topic_count, forums(:cars).topics.count
   end
+
+  test 'self_and_descendents returns the forum and all children of any depth' do
+    assert_equal forums(:cars).self_and_descendents.count, Forum.count
+    assert_equal forums(:model_s).self_and_descendents.count, 3
+    assert_equal forums(:model_s_2015).self_and_descendents.count, 1
+  end
+
+  test 'self_and_desc_topic_count returns num of topics in forum and all children' do
+    assert_equal forums(:cars).self_and_desc_topic_count, Topic.count
+    assert_equal forums(:model_s).self_and_desc_topic_count, 3
+    assert_equal forums(:model_s_2015).self_and_desc_topic_count, 1
+  end
+
+  test 'post_count returns num of posts in the forum' do
+    assert_equal forums(:cars).post_count, 2
+  end
+
+  test 'self_and_desc_post_count returns num of posts in forum and all children' do
+    assert_equal forums(:cars).self_and_desc_post_count, Post.count
+    assert_equal forums(:model_s).self_and_desc_post_count, 6
+    assert_equal forums(:model_s_2015).self_and_desc_post_count, 2
+  end
+
+  test 'last_topic returns the last topic created in the forum' do
+    topic = Topic.where(forum: forums(:cars)).order(created_at: :desc).first
+    assert_equal forums(:cars).last_topic, topic
+  end
+
+  test 'last_post_in_last_topic returns the last post in the last topic created in the forum' do
+    post = forums(:cars).last_topic.posts.order(created_at: :desc).first
+    assert_equal forums(:cars).last_post_in_last_topic, post
+  end
+
+  # test 'tree_sql_for returns SQL to get forum and all children' do
+  # end
+
+  test 'tree_for scope returns forum and all children' do
+    assert_equal Forum.tree_for(forums(:cars)).count, Forum.count
+  end
 end
