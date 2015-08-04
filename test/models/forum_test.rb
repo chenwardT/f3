fb54c87453fd6611ef3d387377b9ef378ff28d4c
Edward Chen <chenward.T@gmail.com>
2015-08-04 14:11:59 -0400

    Fix post fixtures sometimes not loading, cleanup tests, and setup DB properly in travis

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -2,56 +2,56 @@ require 'test_helper'
 
 class ForumTest < ActiveSupport::TestCase
   test 'forum hierarchy' do
-    assert_equal forums(:cars).forums, [forums(:na), forums(:hybrids), forums(:electric)]
+    assert_equal [forums(:na), forums(:hybrids), forums(:electric)], forums(:cars).forums
     assert_includes forums(:hybrids).forums, forums(:prius)
     assert_includes forums(:electric).forums, forums(:model_s)
-    assert_equal forums(:model_s).forums, [forums(:model_s_2014), forums(:model_s_2015)]
+    assert_equal [forums(:model_s_2014), forums(:model_s_2015)], forums(:model_s).forums
   end
 
   test 'to_s returns title' do
-    assert_equal forums(:cars).to_s, forums(:cars).title
+    assert_equal forums(:cars).title, forums(:cars).to_s
   end
 
   test 'topic_count returns count of nested topics' do
-    assert_equal forums(:cars).topic_count, forums(:cars).topics.count
+    assert_equal forums(:cars).topics.count, forums(:cars).topic_count
   end
 
   test 'self_and_descendents returns the forum and all children of any depth' do
-    assert_equal forums(:cars).self_and_descendents.count, Forum.count
-    assert_equal forums(:model_s).self_and_descendents.count, 3
-    assert_equal forums(:model_s_2015).self_and_descendents.count, 1
+    assert_equal Forum.count, forums(:cars).self_and_descendents.count
+    assert_equal 3, forums(:model_s).self_and_descendents.count
+    assert_equal 1, forums(:model_s_2015).self_and_descendents.count
   end
 
   test 'self_and_desc_topic_count returns num of topics in forum and all children' do
-    assert_equal forums(:cars).self_and_desc_topic_count, Topic.count
-    assert_equal forums(:model_s).self_and_desc_topic_count, 3
-    assert_equal forums(:model_s_2015).self_and_desc_topic_count, 1
+    assert_equal Topic.count, forums(:cars).self_and_desc_topic_count
+    assert_equal 3, forums(:model_s).self_and_desc_topic_count
+    assert_equal 1, forums(:model_s_2015).self_and_desc_topic_count
   end
 
   test 'post_count returns num of posts in the forum' do
-    assert_equal forums(:cars).post_count, 2
+    assert_equal 2, forums(:cars).post_count
   end
 
   test 'self_and_desc_post_count returns num of posts in forum and all children' do
-    assert_equal forums(:cars).self_and_desc_post_count, Post.count
-    assert_equal forums(:model_s).self_and_desc_post_count, 6
-    assert_equal forums(:model_s_2015).self_and_desc_post_count, 2
+    assert_equal Post.count, forums(:cars).self_and_desc_post_count
+    assert_equal 6, forums(:model_s).self_and_desc_post_count
+    assert_equal 2, forums(:model_s_2015).self_and_desc_post_count
   end
 
   test 'last_topic returns the last topic created in the forum' do
     topic = Topic.where(forum: forums(:cars)).order(created_at: :desc).first
-    assert_equal forums(:cars).last_topic, topic
+    assert_equal topic, forums(:cars).last_topic
   end
 
   test 'last_post_in_last_topic returns the last post in the last topic created in the forum' do
     post = forums(:cars).last_topic.posts.order(created_at: :desc).first
-    assert_equal forums(:cars).last_post_in_last_topic, post
+    assert_equal post, forums(:cars).last_post_in_last_topic
   end
 
   # test 'tree_sql_for returns SQL to get forum and all children' do
   # end
 
   test 'tree_for scope returns forum and all children' do
-    assert_equal Forum.tree_for(forums(:cars)).count, Forum.count
+    assert_equal Forum.count, Forum.tree_for(forums(:cars)).count
   end
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -2,33 +2,35 @@ require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
   test 'to_s returns title' do
-    assert_equal Topic.first.to_s, Topic.first.title
+    assert_equal Topic.first.title, Topic.first.to_s
   end
 
   test 'reply_count returns number of replies in topic' do
-    assert_equal Topic.first.reply_count, Topic.first.posts.count - 1
+    assert_equal Topic.first.posts.count - 1, Topic.first.reply_count
   end
 
   test 'last_post returns last post created in topic' do
     topic = Topic.first
-    assert_equal topic.last_post, Post.where(topic: topic).order(created_at: :desc).first
+    assert_equal Post.where(topic: topic).order(created_at: :desc).first, topic.last_post
   end
 
   test 'ordered_posts returns posts in ascending order by creation time' do
     topic = Topic.first
+    topic.posts.create!(user: users(:alice), body: 'body')
+    sleep(1)
     topic.posts.create!(user: users(:alice), body: 'body')
     assert topic.ordered_posts.first.created_at < topic.ordered_posts.last.created_at
   end
 
   test 'num_pages returns the number of pages for a given post' do
     topic = Topic.first
-    assert_equal topic.num_pages, 1
+    assert_equal 1, topic.num_pages
 
     15.times do
       topic.posts.create!(user: users(:alice), body: 'body')
     end
 
     topic.reload
-    assert_equal topic.num_pages, 2
+    assert_equal 2, topic.num_pages
   end
 end
