6bcdbfad9cbc148a62ce8f452bc03d7e23083f3d
Edward Chen <chenward.T@gmail.com>
2015-08-06 06:06:12 -0400

    Configure test_helper to include Devise::TestHelpers for controller tests, include FactoryGirl methods in all tests, setup for minitest-rails, don't try and load fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,25 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require 'minitest/reporters'
+ENV["RAILS_ENV"] = "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "minitest/rails"
 
+require "minitest/reporters"
 MiniTest::Reporters.use!
+
+# To add Capybara feature tests add `gem "minitest-rails-capybara"`
+# to the test group in the Gemfile and uncomment the following:
+# require "minitest/rails/capybara"
+
+# Uncomment for awesome colorful output
+# require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-  include FactoryGirl::Syntax::Methods
+  # fixtures :all
   # Add more helper methods to be used by all tests here...
+  include FactoryGirl::Syntax::Methods
+end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
