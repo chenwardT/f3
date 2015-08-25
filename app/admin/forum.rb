ff2b1637714a2f5ba7071770cbbf702bbc2e0d1c
Edward Chen <chenward.T@gmail.com>
2015-08-25 19:44:32 -0400

    Update admin/forums#show and #edit

--- app/admin/forum.rb
+++ app/admin/forum.rb
@@ -7,5 +7,34 @@ ActiveAdmin.register Forum do
     actions
   end
 
+  show :title => proc {|forum| forum.title} do
+    attributes_table do
+      row :id
+      row("Parent Forum") { |forum| forum.forum }
+      row :title
+      row :slug
+      row :description
+      row :views_count
+      row :locked
+      row :hidden
+      row :created_at
+      row :updated_at
+    end
+  end
+
+  permit_params :title, :description, :forum, :locked, :hidden
+
+  form do |f|
+    f.inputs "Forum" do
+      f.input :title
+      f.input :description
+      f.input :forum, label: "Parent Forum"
+      f.input :locked
+      f.input :hidden
+    end
+
+    f.actions
+  end
+
   config.filters = false
 end
