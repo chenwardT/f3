65763457d509c803181f2c7e8b677190720bb393
Edward Chen <chenward.T@gmail.com>
2015-08-26 13:58:03 -0400

    Add method to ForumPermission model to get effective permissions by following inheritance

--- app/models/forum_permission.rb
+++ app/models/forum_permission.rb
@@ -1,4 +1,15 @@
 class ForumPermission < ActiveRecord::Base
   belongs_to :forum
   belongs_to :group
+
+  def effective_permissions
+    source = self
+
+    while source.inherit
+      # "if source.forum.forum" only needed if root doesn't specify inherit = false
+      source = source.forum.parent.forum_permissions.find_by(group: group) if source.forum.parent
+    end
+
+    source
+  end
 end
