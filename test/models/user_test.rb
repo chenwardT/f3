9f8acea3e2c5089751e0de21bf96e998bff31a6c
Edward Chen <chenward.T@gmail.com>
2015-08-28 15:09:27 -0400

    Convert User test to spec and include current model methods

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,16 +1,44 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = FactoryGirl.create(:user)
+describe User do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:group) { FactoryGirl.create(:group) }
+  let(:forum_1) { FactoryGirl.create(:forum) }
+  let(:forum_2) { FactoryGirl.create(:forum, forum: forum_1) }
+
+  before do
+    user.groups << group
   end
 
-  test 'user to_s returns username' do
-    assert_equal @user.to_s, @user.username
+  it "returns the username" do
+    user.to_s.must_equal user.username
   end
 
-  test 'newest scope should return last created username' do
-    @new_user = FactoryGirl.create(:user, username: 'the new guy')
-    assert_equal User.newest, @new_user.username
+  it "can be scoped by creation date, ascending" do
+    new_user = FactoryGirl.create(:user)
+    User.newest.must_equal new_user.username
+  end
+
+  it "returns populated profile fields" do
+    user.profile_fields.sort.must_equal [:country, :bio, :quote, :website, :signature].sort
+  end
+
+  it "checks permission given an action and a forum" do
+    fp = ForumPermission.find_by(forum: forum_2, group: group)
+    fp.update_attributes(inherit: false, view_forum: false)
+
+    user.able_to?(:view_forum, forum_2).must_equal false
+
+    fp.update_attribute(:view_forum, true)
+
+    user.able_to?(:view_forum, forum_2).must_equal true
+  end
+
+  it "returns whether it's an admin" do
+    user.is_admin?.must_equal false
+
+    user.groups.first.update_attribute(:admin, true)
+
+    user.is_admin?.must_equal true
   end
 end
