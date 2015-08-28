4d590db98aadcf92d86547fcff51bab9ba06d43e
Edward Chen <chenward.T@gmail.com>
2015-08-28 15:08:50 -0400

    Create Group spec

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
-class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Group do
+  let(:group) { FactoryGirl.create(:group, name: 'some name') }
+
+  before do
+    3.times do
+      user = FactoryGirl.create(:user)
+      user.groups << group
+    end
+  end
+
+  it "returns the group's name" do
+    group.to_s.must_equal 'some name'
+  end
+
+  it "returns the count of users in the group" do
+    group.user_count.must_equal 3
+  end
 end
