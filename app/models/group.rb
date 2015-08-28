486d4e9cbd129dd395fa603baa5a4c8f2de1d90a
Edward Chen <chenward.T@gmail.com>
2015-08-28 03:41:27 -0400

    Add after_create and before_destroy callbacks to Group model for handling associated ForumPermissions

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,11 +5,28 @@ class Group < ActiveRecord::Base
   has_many :users, through: :user_groups
   has_many :forum_permissions
 
+  after_create :create_forum_permissions
+  before_destroy :destroy_forum_permissions
+
   def to_s
     name
   end
 
   def user_count
     users.count
+  end
+
+  private
+
+  def create_forum_permissions
+    ActiveRecord::Base.transaction do
+      Forum.all.each do |forum|
+        ForumPermission.create(forum: forum, group: self)
+      end
+    end
+  end
+
+  def destroy_forum_permissions
+    ForumPermission.where(group: self).destroy_all
   end
 end
