351230c8fdf22744da82597edb348873cb09701a
Edward Chen <chenward.T@gmail.com>
2015-08-31 10:34:12 -0400

    Cleanup PostsController spec

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,81 +1,110 @@
 require 'test_helper'
 
 describe "PostsController" do
-  let(:forum) { FactoryGirl.create(:forum) }
   let(:user) { FactoryGirl.create(:user) }
+  let(:forum) { FactoryGirl.create(:forum) }
   let(:topic) { FactoryGirl.create(:topic, forum: forum, user: user)}
+
   let(:post_params) { FactoryGirl.attributes_for(:post, user: user, topic_id: topic.id) }
-  let(:bad_post_params) { FactoryGirl.attributes_for(:post, user: user, topic_id: topic.id, body: 'under10') }
+
+  let(:post_params_too_short) do
+    FactoryGirl.attributes_for(:post, user: user, topic_id: topic.id, body: 'under10')
+  end
+
+  let(:full_perms) do
+    FactoryGirl.create(:group, Group.permission_fields.map { |field| [field, true] }.to_h)
+  end
+
+  let (:no_perms) do
+    FactoryGirl.create(:group, Group.permission_fields.map { |field| [field, false] }.to_h)
+  end
 
   describe "POST :create" do
-    before do
-      sign_in user
-    end
-
-    it "creates the post on the topic" do
-      value { post :create, post: post_params }.must_change 'topic.posts.count'
-    end
-
-    describe "when topic has a full page of posts" do
+    describe "with permission" do
       before do
-        15.times { FactoryGirl.create(:post, topic: topic, user: user) }
+        user.groups << full_perms
+        sign_in user
       end
 
-      it "redirects to last page of topic on post creation" do
-        post :create, post: post_params
-        must_redirect_to "/topics/#{topic.id}?page=2"
+      it "creates the post on the topic" do
+        value { post :create, post: post_params }.must_change 'topic.posts.count'
+        flash[:success].must_equal "Post successfully created"
+        must_redirect_to topic_path(post_params[:topic_id], page: 1)
+      end
+
+      it "notifies user of reason for post failure and redirects" do
+        post :create, post: post_params_too_short
+        flash[:danger].must_equal 'Error posting reply: Body is too short (minimum is 10 characters)'
+      end
+
+      describe "when topic has a full page of posts" do
+        before do
+          POSTS_PER_PAGE.times { FactoryGirl.create(:post, topic: topic, user: user) }
+        end
+
+        it "redirects to last page of topic on post creation" do
+          post :create, post: post_params
+          must_redirect_to topic_path(topic, page: 2)
+        end
+
+        it "redirects to last page of topic on failure to create post" do
+          FactoryGirl.create(:post, topic: topic, user: user)
+          post :create, post: post_params_too_short
+          must_redirect_to topic_path(topic, page: 2)
+        end
       end
     end
 
-    it "confirms post creation via flash" do
-      post :create, post: post_params
-      flash[:success].must_equal 'Post successfully created'
-    end
+    describe "without permission" do
+      before do
+        user.groups << full_perms
+        sign_in user
+        full_perms.update_attribute(:create_post, false)
+      end
 
-    it "redirects to last page of topic on failure to create post" do
-      post :create, post: bad_post_params
-      must_redirect_to "/topics/#{topic.id}?page=0"
-    end
-
-    it "notifies user of reason for post creation failure via flash when too short" do
-      post :create, post: bad_post_params
-      flash[:danger].must_equal 'Error posting reply: Body is too short (minimum is 10 characters)'
+      it "does not allow the post to be created" do
+        value { post :create, post: post_params }.wont_change 'topic.posts.count'
+        must_redirect_to root_path
+        flash[:danger].must_equal "You are not authorized to do that"
+      end
     end
   end
 
   describe "POST :soft_delete" do
-    describe "by authorized user" do
-      let(:admin_group) { FactoryGirl.create(:group, name: 'admin') }
-      let(:admin) { FactoryGirl.create(:user) }
+    describe "with permission" do
       let(:post1) { FactoryGirl.create(:post, topic: topic, user: user) }
       let(:post2) { FactoryGirl.create(:post, topic: topic, user: user) }
+      let(:post3) { FactoryGirl.create(:post, topic: topic, user: user) }
 
       before do
-        admin.groups << admin_group
-        admin.save
-        sign_in admin
+        user.groups << full_perms
+        sign_in user
       end
 
       it "soft deletes referenced posts when sent via AJAX post" do
-        xhr :post, :soft_delete, ids: [post1.id, post2.id]
+        xhr :post, :soft_delete, ids: [post2.id, post3.id]
 
-        must_render_template nil
-        Post.where(state: 'deleted').count.must_equal 2
+        response.content_type.must_equal Mime::JS
+        response.body.must_equal "location.reload();"
+        Post.where(id: [post2.id, post3.id]).each { |post| post.state.must_equal 'deleted' }
       end
 
       it "redirects to forum index when not requested via AJAX" do
-        post :soft_delete, ids: [post1.id, post2.id]
+        post :soft_delete, ids: [post2.id, post3.id]
 
         must_redirect_to forums_path
       end
     end
 
-    describe "by unauthorized user" do
+    describe "without permission" do
       let(:post1) { FactoryGirl.create(:post, topic: topic, user: user) }
       let(:post2) { FactoryGirl.create(:post, topic: topic, user: user) }
+      let(:post3) { FactoryGirl.create(:post, topic: topic, user: user) }
 
       before do
+        user.groups << full_perms
         sign_in user
+        full_perms.update_attributes(soft_delete_own_post: false, soft_delete_any_post: false)
       end
 
       it "redirects to forum index and displays error in flash" do
@@ -93,17 +122,13 @@ describe "PostsController" do
     let(:post2) { FactoryGirl.create(:post, topic: topic, user: user, state: 'deleted') }
     let(:post3) { FactoryGirl.create(:post, topic: topic, user: user, state: 'unapproved')}
 
-    describe "by authorized user" do
-      let(:admin_group) { FactoryGirl.create(:group, name: 'admin') }
-      let(:admin) { FactoryGirl.create(:user) }
-
+    describe "with permission" do
       before do
-        admin.groups << admin_group
-        admin.save
-        sign_in admin
+        user.groups << full_perms
+        sign_in user
       end
 
-      it "sets state to visible on all soft-deleted posts" do
+      it "sets state to visible on all soft deleted posts" do
         xhr :post, :undelete, ids: [post1.id, post2.id]
 
         must_render_template nil
@@ -111,7 +136,7 @@ describe "PostsController" do
         post2.reload.state.must_equal 'visible'
       end
 
-      it "does not touch state of posts that weren't soft-deleted" do
+      it "does not touch state of posts that weren't soft deleted" do
         xhr :post, :undelete, ids: [post1.id, post3.id]
 
         must_render_template nil
@@ -120,9 +145,11 @@ describe "PostsController" do
       end
     end
 
-    describe "by unauthorized user" do
+    describe "without permission" do
       before do
+        user.groups << full_perms
         sign_in user
+        full_perms.update_attributes(soft_delete_own_post: false, soft_delete_any_post: false)
       end
 
       it "responds with JS page reload and displays error in flash" do
