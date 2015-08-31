ff684e81dafd66aeb61b6c4696e90fbcfefaa5e8
Edward Chen <chenward.T@gmail.com>
2015-08-31 14:42:44 -0400

    Add permission-aware blocks to ForumsController spec

--- test/controllers/forums_controller_test.rb
+++ test/controllers/forums_controller_test.rb
@@ -1,65 +1,79 @@
 require 'test_helper'
 
-describe "ForumsController" do
+describe ForumsController do
+  let(:forum) { Forum.first }
+  let(:user) { FactoryGirl.create(:user) }
+
+  let(:full_perms) do
+    FactoryGirl.create(:group, Group.permission_fields.map { |field| [field, true] }.to_h)
+  end
+
+  let (:no_perms) do
+    FactoryGirl.create(:group, Group.permission_fields.map { |field| [field, false] }.to_h)
+  end
+
   before do
     3.times { FactoryGirl.create(:forum) }
   end
 
-  let(:forum) { Forum.first }
-
   describe "GET :index" do
-    before do
-      get :index
+    describe "with permssion" do
+      before do
+        user.groups << full_perms
+        get :index
+      end
+
+      it "renders forums/index" do
+        must_render_template "forums/index"
+      end
+
+      it "responds with success" do
+        must_respond_with :success
+      end
+
+      it "includes all top-level forums" do
+        assigns(:top_level_forums).count.must_equal 3
+      end
     end
 
-    it "renders forums/index" do
-      must_render_template "forums/index"
-    end
-
-    it "responds with success" do
-      must_respond_with :success
-    end
-
-    it "includes all top-level forums" do
-      assigns(:top_level_forums).count.must_equal 3
-    end
+    # TODO: Write this once filtering by permissions on index for user is implemented.
+    # describe "without permission" do
+    # end
   end
 
-  describe "GET :show without logged in user" do
-    before do
-      get :show, {id: forum.id}
+  describe "GET :show" do
+    describe "with permission" do
+      let(:forum) { FactoryGirl.create(:forum) }
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        user.groups << full_perms
+        sign_in user
+        get :show, { id: forum.id }
+      end
+
+      # TODO: Why is this seeing views_count == 2 after supposedly being viewed once?
+      it "registers a global view" do
+        forum.reload.views_count.wont_equal 0
+      end
+
+      it "registers a user-specific view" do
+        forum.reload.view_for(user).count.must_equal 1
+      end
     end
 
-    it "renders forums/show" do
-      must_render_template "forums/show"
-    end
+    describe "without permission" do
+      before do
+        get :show, {id: forum.id}
+      end
 
-    it "responds with success" do
-      must_respond_with :success
-    end
+      it "redirects to root" do
+        must_redirect_to root_path
+      end
 
-    it "doesn't register a global view when no user is logged in" do
-      forum.views_count.must_equal 0
-    end
-  end
-
-  describe "GET :show with user logged in" do
-    let(:forum) { FactoryGirl.create(:forum) }
-    let(:user) { FactoryGirl.create(:user) }
-
-    before do
-      forum.reload.views_count.must_equal 0
-      sign_in user
-      get :show, {id: forum.id }
-    end
-
-    # TODO: Why is this seeing views_count == 2 after supposedly being viewed once?
-    it "registers a global view" do
-      forum.reload.views_count.wont_equal 0
-    end
-
-    it "registers a user-specific view" do
-      forum.reload.view_for(user).count.must_equal 1
+      it "doesn't register a global view when no user is logged in" do
+        forum.views_count.must_equal 0
+      end
     end
   end
 end
