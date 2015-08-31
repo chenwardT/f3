e260969977e5d4203c451c1655827a7c4be6be9c
Edward Chen <chenward.T@gmail.com>
2015-08-31 08:33:11 -0400

    Add spec for Group create/destroy callbacks

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -17,4 +17,22 @@ describe Group do
   it "returns the count of users in the group" do
     group.user_count.must_equal 3
   end
+
+  describe "handling associated forum permission sets" do
+    let(:forum) { FactoryGirl.create(:forum) }
+
+    it "creates a set of forum permissions on creation of itself" do
+      value do
+        3.times { FactoryGirl.create(:group) }
+      end.must_change "ForumPermission.where(forum: forum).count", 3
+    end
+
+    it "destroys associated forum permissions on destruction of itself" do
+      value do
+        # TODO: Cleanup when cascading deletion implemented.
+        group.users.each { |user| user.groups.delete(group) }
+        group.destroy
+      end.must_change "ForumPermission.where(forum: forum).count", -1
+    end
+  end
 end
