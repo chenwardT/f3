ba60f426836cc76f3f6e6b1668afa7192af3de9c
Edward Chen <chenward.T@gmail.com>
2015-08-31 09:09:12 -0400

    Convert Viewable test to spec. Disabled due to module specs not setup.

--- test/models/concerns/viewable_test.rb
+++ test/models/concerns/viewable_test.rb
@@ -48,3 +48,55 @@ class ViewableTest < ActiveSupport::TestCase
     assert_nil @top_topic.views_count
   end
 end
+
+# FIXME: Module specs not working: must be run within a transaction (register_spec_type?)
+# and some spec methods not being recognized (must_change, wont_change).
+
+# describe Viewable do
+#   let(:user) { FactoryGirl.create(:user) }
+#
+#   let(:top) { FactoryGirl.create(:forum) }
+#   let(:top_topic) { FactoryGirl.create(:topic, forum: top, user: user) }
+#
+#   before do
+#     FactoryGirl.create(:post, topic: top_topic, user: user)
+#     FactoryGirl.create(:post, topic: top_topic, user: user)
+#   end
+#
+#   it "returns the number of times a user viewed itself" do
+#     top_topic.view_for(user).must_be_nil
+#
+#     top_topic.register_view_by(user)
+#
+#     top_topic.view_for(user).count.must_equal 1
+#   end
+#
+#   it "can increment view counter and update times" do
+#     top_topic.view_for(user).must_be_nil
+#
+#     top_topic.register_view_by(user)
+#
+#     value do
+#       top_topic.register_view_by(user)
+#     end.must_change "top_topic.views_count"
+#
+#     # User-specific view count increment
+#     value do
+#       top_topic.register_view_by(user)
+#     end.must_change "top_topic.view_for(user).count"
+#
+#     value do
+#       top_topic.register_view_by(user)
+#     end.must_change "top_topic.view_for(user).current_viewed_at"
+#   end
+#
+#   it "doesn't register a view if no user is supplied" do
+#     top_topic.view_for(user).must_be_nil
+#
+#     value do
+#       top_topic.register_view_by(nil)
+#     end.wont_change "View.count"
+#
+#     top_topic.views_count.must_be_nil
+#   end
+# end
