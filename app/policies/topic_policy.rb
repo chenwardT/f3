25d7ae62defadae56c3f38b29d1349053efad767
Edward Chen <chenward.T@gmail.com>
2015-09-01 11:26:18 -0400

    Add lock, unlock, copy, move to TopicPolicy

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -14,4 +14,32 @@ class TopicPolicy < ApplicationPolicy
   def moderate?
     user && user.able_to?(:moderate_any_forum, record.forum)
   end
+
+  def lock?
+    if user
+      if user.able_to?(:lock_or_unlock_any_topic, record.forum)
+        true
+      else
+        record.author == user && user.able_to?(:lock_or_unlock_own_topic, record.forum)
+      end
+    end
+  end
+
+  def unlock?
+    lock?
+  end
+
+  def copy?
+    if user
+      if user.able_to?(:copy_or_move_any_topic, record.forum)
+        true
+      else
+        record.author == user && user.able_to?(:copy_or_move_own_topic, record.forum)
+      end
+    end
+  end
+
+  def move?
+    copy?
+  end
 end
