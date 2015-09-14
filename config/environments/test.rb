556dfd3604038250312ba36ee5b2d708a03b4d6a
Edward Chen <chenward.T@gmail.com>
2015-09-14 16:13:53 -0400

    Include default_url_options in environments/test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.active_support.test_order = :random
+
+  # devise setup
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
